6d1bc099e4f446927e4245aec1a6d0f2ef878887
mauroff3d <mauro.maggioni@ff3d.com>
2014-11-18 15:22:06 +0100

    modifica pollutantmean fun

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,7 +1,8 @@
 pollutantmean<-function(directory, pollutant, id=1:332){
   tab<-list.files(directory, full.name=TRUE)
+  app<-data.frame()
   for(i in id){
-    app<-read.csv(tab[i])
-    
+    app<-rbind(app, read.csv(tab[i]))    
   }
+  return(mean(app[,pollutant], na.rm=TRUE))
 }
